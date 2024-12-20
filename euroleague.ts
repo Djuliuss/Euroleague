165f501e8b0e8cb3309e83727124a35b81b64080
Julio Delgado <juliods@gmail.com>
2024-12-20 10:16:35 +0000

    export data

--- euroleague.ts
+++ euroleague.ts
@@ -1,50 +1,12 @@
 // TypeScript version of the game
 import { readUserInput } from "./file";
-
-// Define Player interface
-interface Player {
-  name: string;
-  team: number;
-}
+import { players } from "./data";
 
 // Player data
 
 // 1 - Monaco
 // 2 - Olympiacos
 // 3 - Red Star
-
-const players: Player[] = [
-  { name: "MIKE JAMES", team: 1 },
-  { name: "ALPHA DIALLO", team: 1 },
-  { name: "ELIE OKOBO", team: 1 },
-  { name: "JARON BLOSSOMGAME", team: 1 },
-  { name: "JORDAN LOYD", team: 1 },
-  { name: "GEORGIOS PAPAGIANNIS", team: 1 },
-  { name: "DONATAS MOTIEJUNAS", team: 1 },
-  { name: "MATTHEW STRAZEL", team: 1 },
-  { name: "MAM JAITEH", team: 1 },
-  { name: "VITTO BROWN", team: 1 },
-  { name: "SASHA VEZENKOV", team: 2 },
-  { name: "EVAN FOURNIER", team: 2 },
-  { name: "THOMAS WALKUP", team: 2 },
-  { name: "NIKOLA MILUTINOV", team: 2 },
-  { name: "NIGEL WILLIAMS-GOSS", team: 2 },
-  { name: "LUCA VILDOZA", team: 2 },
-  { name: "ALEC PETERS", team: 2 },
-  { name: "SHAQUIELLE MCKISSIC", team: 2 },
-  { name: "KOSTAS PAPANIKOLAOU", team: 2 },
-  { name: "MOUSTAPHA FALL", team: 2 },
-  { name: "FILIP PETRUSEV", team: 3 },
-  { name: "JOEL BOLOMBOY", team: 3 },
-  { name: "NIKOLA KALINIC", team: 3 },
-  { name: "CODI MILLER-MCINTYRE", team: 3 },
-  { name: "YAGO DOS SANTOS", team: 3 },
-  { name: "NEMANJA NEDOVIC", team: 3 },
-  { name: "LUKA MITROVIC", team: 3 },
-  { name: "ISAIAH CANAAN", team: 3 },
-  { name: "MIKE DAUM", team: 3 },
-  { name: "DEJAN DAVIDOVAC", team: 3 },
-];
 
 let streak: number = 0; // Streak counter
 let streakTarget: number = 100; // Current player index
